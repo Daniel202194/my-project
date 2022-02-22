eaddce53261e030d1d6b5e2b308b0e12dde48c0a
Administrator <kolbasov@post.bgu.ac.il>
2022-02-22 10:43:18 +0200

    new deploy

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from flask_ngrok import run_with_ngrok
 from flask import Flask, request, jsonify
 
-class MyFlaskApp(Flask):
-    def run(self, host=None, port=None, debug=None, **options):
-        super(MyFlaskApp, self).run(host=host, port=port, debug=debug, **options)
+# class MyFlaskApp(Flask):
+#     def run(self, host=None, port=None, debug=None, **options):
+#         super(MyFlaskApp, self).run(host=host, port=port, debug=debug, **options)
 
 
-app = MyFlaskApp(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+# application = MyFlaskApp(__name__)
+# application.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def show_hello():
-    return '<p>hello world<p>'
-if __name__ == '__main__':
-    # run the Flask RESTful API, make the server publicly available (host='0.0.0.0') on port 8080
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    return 'hello world'
+# if __name__ == '__main__':
+#     # run the Flask RESTful API, make the server publicly available (host='0.0.0.0') on port 8080
+#     application.run(host='0.0.0.0', port=8080, debug=False)
     #run_with_ngrok(app)
     #app.run()
